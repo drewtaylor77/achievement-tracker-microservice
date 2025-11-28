88c33ffb641b9a2f64d8974270aa344dc301fd0e
Drew Taylor <121739743+drewtaylor77@users.noreply.github.com>
2025-11-28 12:30:32 -0800

    Update client.py

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ print(f'Achievement Unlocked: {message}')
 # Get achievements
 socket.send_json({'request': 'get_achievements', 'user_id': 'user1'})
 message = socket.recv_json()
-print(f'Received reply: {message}')
+print(f'Completed achievements: {message}')
 
 # Shut down server
 socket.send_json({'request': 'Q'})
